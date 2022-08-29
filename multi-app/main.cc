620a1d3cc102cc84414bc5572ce48234adbcffd2
JeanTracker <hyojoong.kim.jean@gmail.com>
2022-08-29 12:07:14 +0900

    rename samples
        gui: gui sample application
        extension: extension sample for standalone application
        daemon: daemon sample for multi application
    
    Signed-off-by: JeanTracker <hyojoong.kim.jean@gmail.com>

--- multi-app/main.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int argc, char* args[])
-{
-    std::cout << "multi application example" << std::endl;
-    return 1;
-}
--- single-app/main.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int argc, char* args[])
-{
-    std::cout << "single application example" << std::endl;
-    return 1;
-}
